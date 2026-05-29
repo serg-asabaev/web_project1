ce73d986a74738b808e135305f6951976e5c632a
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-05-29 22:45:59 +0400

    added python server

--- src/http_server.py
+++ src/http_server.py
@@ -6,13 +6,13 @@ class MyServer(BaseHTTPRequestHandler):
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
-    def do_get(self):
+    def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
         self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "application/html") # Отправка типа данных, который будет передаваться
+        self.send_header('Content-type', 'text/html; charset=utf-8') # Отправка типа данных, который будет передаваться
         self.end_headers() # Завершение формирования заголовков ответа
 
         with open('static/contacts.html', "r", encoding="utf-8") as f:
-            html_page = f.read()
+            html = f.read()
 
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа
+        self.wfile.write(html.encode('utf-8'))  # Тело ответа
