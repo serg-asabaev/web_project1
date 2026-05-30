05dd7c3e1511a9590c64fab81e066c4ed2a0ee53
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-05-30 22:57:22 +0400

    degug paths in app

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("👋 Главная страница каталога работает!")
+
+def contacts(request):
+    return render(request, 'contacts.html')
