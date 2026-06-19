acc51177a7651ae7b5ab3befd3137d74377995ea
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-06-19 23:51:17 +0400

    added new html pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/details/<int:pk>/', views.product_details, name='product_details'),
+    path('product/list/', views.product_list, name='products_list')
 ]
