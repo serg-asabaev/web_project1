66dadd1b50fbfccfc4a9c671ffd6f634e9b88b85
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-06-20 00:44:01 +0400

    added inner links between pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacts/', views.contacts, name='contacts'),
-    path('product/details/<int:pk>/', views.product_details, name='product_details'),
-    path('product/list/', views.product_list, name='products_list')
+    path('products/detail/<int:pk>/', views.product_details, name='products_detail'),
+    path('products/list/', views.product_list, name='products_list')
 ]
