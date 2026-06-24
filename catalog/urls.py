fa58b9287dc027dfad56d20bb43d0b75b6fa1c5d
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-06-24 23:18:47 +0400

    change fbv to cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('products/detail/<int:pk>/', views.product_details, name='products_detail'),
-    path('products/list/', views.product_list, name='products_list')
+    path('', views.IndexView.as_view(), name='index'),
+    path('contacts/', views.ContactsView.as_view(), name='contacts'),
+    path('products/<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('products/list/', views.ProductListView.as_view(), name='product_list')
 ]
