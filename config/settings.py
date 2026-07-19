dc957ded57d36c78e058976f94102c0c4f7f56ba
Сергей Асабаев <serg-asabaev@yandex.ru>
2026-07-19 20:57:34 +0400

    added sending welcome email and authorized access to add, delete, etc

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,14 @@ AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'usr123qwe@yandex.ru'
+EMAIL_HOST_PASSWORD = 'cahudyvytebyrrgf'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+#2it-G5b-5k2-UsV
